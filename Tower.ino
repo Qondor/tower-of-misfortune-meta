00494e2de44b0da2bd3607b7179b4f08ad0a9e1d
Qondor <kontakt@cezarywas.pl>
2021-12-19 15:34:21 +0100

    Beta 1.3 - highscore sorting implemented and code clean up

--- Tower.ino
+++ Tower.ino
@@ -1,12 +1,8 @@
 #include <Gamebuino-Meta.h>
 #include <utility>
-// #include <algorithm>
-#include <stdlib.h>  //qsort
 #include <cstdlib>
-#include <time.h>
-#include <ctime>
 
-// ver. beta 1.2
+// ver. beta 1.3
 
 // CONSTANTS //
 float current_brick_position = 10;
@@ -14,7 +10,6 @@ float brick_speed = 0.2;
 int value = 0;
 int b = 160;
 byte mode = 0;
-// bool alive = false;
 int brick_x = 70;
 int score = 0;
 int brick_y = 44;
@@ -29,26 +24,13 @@ char player_name[13];
 String GOtext = "";
 String coded_score = "";
 String temp_score = "";
-
-// char highscore[6][2] = {{"CEZARY - ", 300}, {"BARTEK - ", 257}, {"JAKUB% - ", 256}, {"MATI99 - ", 128}, {"GRUNIO - ", 100}, {"", 000}};
+bool done = false;
 String hs_names[6] = {"CEZARY - ", "BARTEK - ", "JAKUB% - ", "MATI99 - ", "GRUNIO - ", ""};
 int hs_points[6] = {300, 257, 256, 128, 100, 0};
 
-// for compare in qsort
-int compare (const void * a, const void * b)
-{
-  return ( *(int*)a < *(int*)b );
-}
-
-// pair <String, int> hs_1;
-// hs_1.name = hs_names[0];
-// hs_1.name = hs_points[0];
-
-//gb.pickRandomSeed;
 void setup() {
   gb.begin();
   gb.pickRandomSeed();
-
 }
 
 void past_bricks() {
@@ -62,10 +44,9 @@ void loop() {
   gb.display.clear();
   gb.lights.clear();
 
-  // if (!alive) {
   switch (mode) {
+    // TITLE SCREEN //
     case 0:
-      //TITLE SCREEN
       gb.display.clear();
       gb.display.setCursor(gb.display.width() - 10, 0);
       for (int a = 0; a <= gb.display.width(); a = a + 7) {
@@ -73,23 +54,28 @@ void loop() {
         b = b + 20;
         gb.display.fillRect(a, 0, 5, 64);
       }
-      gb.display.setColor(255, 255, 255);
-      // Height -64 width-80
+      
+      gb.display.setColor(255, 255, 255); // Height -64 width-80
 
       gb.display.setCursor(gb.display.width() - 78, gb.display.height() - 60);
       gb.display.print("TOWER OF MISFORTUNE");
-
-      gb.display.setCursor(gb.display.width() - 54, gb.display.height() - 40);
+      
+      gb.display.setCursor(gb.display.width() - 54, gb.display.height() - 45);
       gb.display.print("A: Start!");
 
-      gb.display.setCursor(gb.display.width() - 54, gb.display.height() - 30);
+      gb.display.setCursor(gb.display.width() - 54, gb.display.height() - 38);
       gb.display.print("B: Help");
 
-      gb.display.setCursor(gb.display.width() - 74, gb.display.height() - 20);
+      gb.display.setCursor(gb.display.width() - 74, gb.display.height() - 31);
       gb.display.print("SELECT: Highscore");
-      gb.waitForUpdate();
-      gb.waitForUpdate();
 
+      gb.display.setCursor(gb.display.width() - 78, gb.display.height() - 15);
+      gb.display.print("(C) Vellichor Games");
+
+      gb.display.setCursor(gb.display.width() - 72, gb.display.height() - 8);
+      gb.display.print("Cezary Was | 2022");
+
+      gb.waitForUpdate();
       if (gb.buttons.released(BUTTON_A)) {
         mode = 4;
       }
@@ -100,10 +86,11 @@ void loop() {
 
       if (gb.buttons.released(BUTTON_MENU)) {
         mode = 3;
+      }
         break;
 
+      // STORY //
       case 1:
-        // STORY //
         gb.display.clear();
         gb.display.setColor(255, 255, 255);
         gb.display.setCursor(30, 5);
@@ -111,14 +98,14 @@ void loop() {
         gb.display.setCursor(5, 10);
         gb.display.print("Welcome to the    Tower of Misfortune.An ancient art of   stacking blocks. The legend says, that's whoever stacks 100 of them shall be     granted a wish. Anything they desire..."); // Okay, thats look like shit. Needs to be repaired.
         // gb.display.print("Press B to continue");
-        gb.waitForUpdate();
+        //gb.waitForUpdate();
         if (gb.buttons.released(BUTTON_B)) {
           mode = 2;
         }
         break;
 
+      // HELP //
       case 2:
-        // HELP //
         gb.display.clear();
         gb.display.setCursor(30, 5);
         gb.display.print("HELP");
@@ -126,48 +113,47 @@ void loop() {
         gb.display.print("Try to match the  previous block and  press A - the better the match, the more points you'll get. Every pixel off will make your block    smaller.");
         gb.display.setCursor(5, 58);
         gb.display.print("Press B to start!");
-        gb.waitForUpdate();
+        //gb.waitForUpdate();
         if (gb.buttons.pressed(BUTTON_B)) {
           mode = 4;
         }
         break;
 
+      // HIGHSCORE //
       case 3:
-        // HIGHSCORE //
         gb.display.clear();
-        gb.display.setCursor(25, 2);
+        gb.display.setCursor(25, 0);
         gb.display.print("HIGHSCORE");
-        gb.display.setCursor(5, 10);
+        gb.display.setCursor(5, 8);
         gb.display.print(hs_names[0]); // CEZARY - 300");
         gb.display.print(hs_points[0]);
-        gb.display.setCursor(5, 16);
+        gb.display.setCursor(5, 14);
         gb.display.print(hs_names[1]); // BARTEK - 257");
         gb.display.print(hs_points[1]);
-        gb.display.setCursor(5, 22);
+        gb.display.setCursor(5, 20);
         gb.display.print(hs_names[2]); // JAKUB% - 256");
         gb.display.print(hs_points[2]);
-        gb.display.setCursor(5, 28);
+        gb.display.setCursor(5, 26);
         gb.display.print(hs_names[3]); // MATI99 - 128");
         gb.display.print(hs_points[3]);
-        gb.display.setCursor(5, 34);
+        gb.display.setCursor(5, 32);
         gb.display.print(hs_names[4]); // GRUNIO - 100");
         gb.display.print(hs_points[4]);
-        gb.display.setCursor(0, 45);
+        gb.display.setCursor(0, 41);
+
         if (coded_score != "") {
-          gb.display.print("Visit ToM.com to upload your result with a code:");
+          gb.display.print("Enter the code on   Vellichor-Void.com/ /ToM to upload your  result: ");
         } else {
           gb.display.print("Play the game first!");
         }
         gb.display.print(coded_score);
-        gb.waitForUpdate();
         if (gb.buttons.pressed(BUTTON_B)) {
           mode = 4;
         }
         break;
 
+      // GAMEPLAY //
       case 4:
-        // GAMEPLAY //
-
         // Score //
         gb.display.setCursor(30, 5);
         gb.display.print("SCORE: ");
@@ -192,7 +178,6 @@ void loop() {
         gb.display.fillRect(0, 53, 80, 1);
 
         current_brick_position = current_brick_position + brick_speed;
-        // gb.display.setColor(255, 211, 0);
 
         // Starting block //
         for (int i = 0; i <= past_brick_pos; i++) {
@@ -240,36 +225,45 @@ void loop() {
             brick_x -= 5;
             past_brick_pos++;
             gb.lights.fill(GREEN);
-            // new_line();
           }
+          
           if (brick_speed == 0) {
-            coded_score = "";
             GOtext = "SELECT: Highscore!";
+
+            // If score is high enough to be put in the Highscore //
             if (score >= (hs_points[4])) {
+              // Ask for a name //
               gb.gui.keyboard("Enter your name!", player_name);
-              srand(time(NULL));
+              srand(time(0));
               temp_score = String(score);
-              coded_score = (int(temp_score[0] - '0') * 2 ) + 1;
-              coded_score += (int(temp_score[1] - '0') * 2 ) + 1;
-              coded_score += (int(temp_score[2] - '0') * 2 ) + 1;
-              coded_score += (int(temp_score[0] - '0') * 2 ) + 1 + (int(temp_score[1] - '0') * 2 ) + 1 + (int(temp_score[2] - '0') * 2 ) + 1 + past_brick_pos;
-              coded_score += rand() % 3;
-              hs_names[5] = player_name;
-              hs_points[5] = score;
-              qsort(hs_points, 6, sizeof(int), compare);
+              coded_score = "";
+              coded_score = (temp_score[0] - '0') * 2 + 10;
+              coded_score += (temp_score[1] - '0') * 2 + 10;
+              coded_score += (temp_score[2] - '0') * 2 + 10;
+              coded_score += (temp_score[0] - '0') + (temp_score[1] - '0') + (temp_score[2] - '0') + 50;
+              coded_score += random(5,9);
+              done = false;
+              for (int which_place = 4; done == false; which_place--) {
+                         if (score >= hs_points[which_place]){
+                            hs_names[which_place + 1] = hs_names[which_place];
+                            hs_points[which_place + 1] = hs_points[which_place];
+                            hs_names[which_place] = String(player_name) += String(" - ");
+                            hs_points[which_place] = score;
+                                                                                                                   
+                            if (which_place == 0 && score >= hs_points[0]){
+                              done = true;
+                              }
+                         }else if (score < hs_points[which_place]){
+                            done = true;
+                         }
+              }
             }
           }
-
           brick_speed *= 1.4;
-          random_red[past_brick_pos] = (rand() % 200) + 50;
-          random_green[past_brick_pos] = (rand() % 200) + 50;
-          random_blue[past_brick_pos] =  (rand() % 200) + 50;
-          // past_brick_pos++;
-          // last_brick_x = brick_x;
-          // last_brick_position = current_brick_position;
-          // last_brick_y = brick_y;
+          random_red[past_brick_pos] = random(50, 200);
+          random_green[past_brick_pos] = random(50, 200);
+          random_blue[past_brick_pos] = random(50, 200);
         }
-        // past_bricks();
         if (gb.buttons.pressed(BUTTON_MENU)) {
           mode = 3;
           brick_x = 70;
@@ -285,9 +279,5 @@ void loop() {
           }
         }
         break;
-
-        // case 5:
       }
   }
-  // }
-}
